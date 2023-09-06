59ac7d1021ec98c20989955fe3a3e862f221e801
Pietro907 <pietrop.puri@gmail.com>
2023-09-06 16:00:47 +0200

    add initial page

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+*/ 
+
+const 
+//MILESTONE 1:
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//MILESTONE 2:
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+/*
+Dati
+Wayne Barnett
+Founder & CEO	
+Angela Caroll	
+Chief Editor	
+Walter Gordon
+Office Manager
+Angela Lopez
+Social Media Manage
+Scott Estrada	
+Developer	
+Barbara Ramos	
+Graphic Designer	
+*/
