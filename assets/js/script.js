afb4acef1948ea0e912722b81f3f8037ae07e57f
Pietro907 <pietrop.puri@gmail.com>
2023-09-06 18:26:57 +0200

    add string information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,45 @@
 /* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 MILESTONE 0:
 Creare l’array di oggetti con le informazioni fornite.
-*/ 
+*/
 
 const studentList = [
     {
-      nome:  'Wayne ',
-      cognome: 'Barnett',
-      job: 'Founder & CEO',
+        nome: 'Wayne ',
+        cognome: 'Barnett',
+        job: 'Founder & CEO',
     },
     {
-      nome: 'Angela', 
-      cognome: 'Caroll',
-      job: 	'Chief Editor',
+        nome: 'Angela',
+        cognome: 'Caroll',
+        job: 'Chief Editor',
     },
     {
-      nome: 'Walter',
-      cognome: ' Gordon',
-      job: 'Office Manager',
-    },
-    {  
-      nome: 'Angela',
-      cognome: ' Lopez',
-      job: 'Social Media Manage',
+        nome: 'Walter',
+        cognome: 'Gordon',
+        job: 'Office Manager',
     },
     {
-      nome: 'Scott',
-      cognome: ' Estrada',
-      job:	'Developer',
+        nome: 'Angela',
+        cognome: 'Lopez',
+        job: 'Social Media Manage',
     },
     {
-      nome: 'Barbara',
-      cognome: ' Ramos',
-      job:	'Graphic Designer',
+        nome: 'Scott',
+        cognome: 'Estrada',
+        job: 'Developer',
+    },
+    {
+        nome: 'Barbara',
+        cognome: ' Ramos',
+        job: 'Graphic Designer',
     }
 
 ];
 console.log(studentList);
 //MILESTONE 1:
 //Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-for(let i = 0; i < studentList.length; i++){
+for (let i = 0; i < studentList.length; i++) {
     const studentInfo = studentList[i];
     console.log(studentInfo['nome']);
     console.log(studentInfo['cognome']);
@@ -49,5 +49,26 @@ for(let i = 0; i < studentList.length; i++){
 
 //MILESTONE 2:
 //Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+
+for (const key in studentList) {
+
+    console.log(key);
+    console.log(studentList[key]);
+}
+
+
+/* cardStudent.insertAdjacentHTML
+(
+    `    
+    
+    `
+); 
+ */
+
+
+
+
 
 
